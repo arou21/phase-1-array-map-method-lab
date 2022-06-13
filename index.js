8da86b9155f3f94afab3b6ac8f0a70fcd648357d
Samuel <smarouba2@gmail.com>
2022-06-13 19:00:02 -0400

    commit tree 3ca40db26a911f37caad52425f2452dd062ae5ee

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+   console.log(tutorial.split(" "))
+   return tutorial.split(" ").map(word => {
+     return word[0].toUpperCase() + word.slice(1)
+    }).join(" ")
+  })
+  }
